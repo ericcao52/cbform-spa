5627beee46208cdfc901a341e103bb61581a23a0
Lissandre <lissandre.pasdeloup@gmail.com>
2024-02-02 11:44:08 +0100

    feat(Head)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,8 +10,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-
+      <main
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "24px",
+          minHeight: "100dvh",
+        }}
+      >
+        <Link href={"/collect"}>Collect</Link>
+        <Link href={"/pots"}>Pots</Link>
       </main>
     </>
   );
